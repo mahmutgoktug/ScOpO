72d6660a9b607fd833aaafb3defad73c81ab1a65
Mahmut Göktuğ <101980878+mahmutgoktug@users.noreply.github.com>
2023-02-28 03:44:07 +0300

    init.

--- anasayfa.js
+++ anasayfa.js
@@ -1,9 +1,5 @@
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 }

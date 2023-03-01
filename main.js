03e17c77d2274ca9f8d550d22bd31de973699fd0
Mahmut Göktuğ <101980878+mahmutgoktug@users.noreply.github.com>
2023-03-01 19:20:03 +0300

    init.

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-50px";
+//   }
+// }
+
+document.getElementById('mode-btn').addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+});
